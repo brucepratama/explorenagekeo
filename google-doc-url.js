8409dac598dbaa39aee33435127ca731f4d20a9c
brucepratama <71397839+brucepratama@users.noreply.github.com>
2020-09-22 13:51:51 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQJM5Z1-vW9w2avlR3LetXoOyIZjPPpbvY9XOYaRnxslBHv0pALizwxdniBbeXkjPHXgWSu37neKCep/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1LiZGbyVEUnUo6eeqj-ChXUW_FEk27JazO7LaJoetAPM/edit#gid=290071338';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
